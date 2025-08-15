dff7b5d8addca3327a4368b12cf77f0fb4aed5a8
nafirahusna <148989630+nafirahusna@users.noreply.github.com>
2025-08-15 08:20:59 +0700

    Update google_service.py

--- services/google_service.py
+++ services/google_service.py
@@ -1,8 +1,5 @@
 import os
 from googleapiclient.discovery import build
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.http import MediaFileUpload
 from datetime import datetime
 
